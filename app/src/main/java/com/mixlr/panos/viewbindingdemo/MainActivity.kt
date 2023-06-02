dd496282e21032fb6209b87616f662d4943c423c
Panos Matsinopoulos <panos@mixlr.com>
2023-06-02 19:32:57 +0300

    Implement the button click - Now works without view bindings

--- app/src/main/java/com/mixlr/panos/viewbindingdemo/MainActivity.kt
+++ app/src/main/java/com/mixlr/panos/viewbindingdemo/MainActivity.kt
@@ -2,10 +2,23 @@ package com.mixlr.panos.viewbindingdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val messageView = findViewById<TextView>(R.id.tvMessage)
+        val personNameView = findViewById<EditText>(R.id.etPersonName)
+        val submitButton = findViewById<Button>(R.id.btnSubmit)
+
+        submitButton.setOnClickListener {
+            val name = personNameView.text.toString()
+            messageView.text = "Hello $name"
+            personNameView.setText("")
+        }
     }
 }
